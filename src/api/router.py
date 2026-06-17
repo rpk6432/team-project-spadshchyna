414bca47856ac65989902048b50de5052dc96f1b
Roman Peka <pekaromanmail@gmail.com>
2026-06-18 01:29:08 +0300

    feat(favourites): add router

--- src/api/router.py
+++ src/api/router.py
@@ -1,10 +1,12 @@
 from fastapi import APIRouter
 
 from auth.router import router as auth_router
+from favourites.router import router as favourites_router
 from homesteads.router import homestead_router, region_router
 
 router = APIRouter(prefix="/api/v1")
 
 router.include_router(auth_router)
+router.include_router(favourites_router)
 router.include_router(homestead_router)
 router.include_router(region_router)
