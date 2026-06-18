355bdb0d6cdf2ef140e83f2fe190989a2207c4ea
Roman Peka <pekaromanmail@gmail.com>
2026-06-18 15:31:41 +0300

    feat(payments): add router

--- src/api/router.py
+++ src/api/router.py
@@ -3,10 +3,12 @@ from fastapi import APIRouter
 from auth.router import router as auth_router
 from favourites.router import router as favourites_router
 from homesteads.router import homestead_router, region_router
+from payments.router import router as payments_router
 
 router = APIRouter(prefix="/api/v1")
 
 router.include_router(auth_router)
 router.include_router(favourites_router)
 router.include_router(homestead_router)
+router.include_router(payments_router)
 router.include_router(region_router)
